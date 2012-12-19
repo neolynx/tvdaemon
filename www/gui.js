c3a88a8a50e613a1ce8fcb897f1e34955ca07c46
André Roth <neolynx@gmail.com>
2012-12-19 17:22:49 +0100

    jquery view

--- www/gui.js
+++ www/gui.js
@@ -1,68 +1,13 @@
+$(document).ready( ready );
 
+window.setInterval( function( ) { ready( ); }, 2000 );
 
-
-var last_adapter = null;
-var last_frontend = null;
-var last_port = null;
-
-function renderAdapter( row )
-{
-  var data = $("#gui").jqxGrid( 'getrowdata', row );
-  adapter = data["adapter"]
-  if( last_adapter == adapter["id"] )
-    return "";
-  last_adapter = adapter["id"];
-  last_frontend = null;
-  type = "???";
-  if( adapter["path"].indexOf( "/usb" ) != -1 )
-    type = "USB";
-  else if ( adapter["path"].indexOf( "/pci" ) != -1 )
-    type = "PCI";
-  if( adapter["present"] == 1 )
-    present = "* ";
-  else
-    present = "! ";
-  return present + type + ": " + adapter["name"];
-}
-
-function renderFrontend( row )
-{
-  var data = $("#gui").jqxGrid( 'getrowdata', row );
-  frontend = data["frontend"]
-  if( last_frontend == frontend["id"] )
-    return "";
-  last_frontend = frontend["id"];
-  last_port = null;
-  return frontend["name"];
-}
-
-function renderPort( row )
-{
-  var data = $("#gui").jqxGrid( 'getrowdata', row );
-  port = data["port"]
-  if( last_port == port["id"] )
-    return "";
-  last_port = port["id"];
-  return port["name"];
-}
-
-function renderSource( row )
-{
-  var data = $("#gui").jqxGrid( 'getrowdata', row );
-  port = data["port"];
-  if( port["source_id"] >= 0 )
-    return sources[port["source_id"]];
-  return "undefined";
-}
-
-var tvdata = [];
+var sources = [];
 
 function ready( )
 {
   $.getJSON('tvd?c=tvdaemon&a=list_sources', readSources );
 }
-
-var sources = [];
 
 function readSources( data )
 {
@@ -75,53 +20,77 @@ function readSources( data )
 
 function readDevices( data )
 {
+  $( '#plant' ).empty( );
   for( a in data )
   {
     adapter = data[a];
+    row = $('<tr>');
+    cell = $('<td>');
+    cell.html( getAdapter( adapter ));
+    row.append( cell );
+
+    adapterports = 0;
     for( f in adapter["frontends"] )
     {
       frontend = adapter["frontends"][f];
+
+      cell2 = $('<td>');
+      cell2.html( getFrontend( frontend ));
+      row.append( cell2 );
+
+      ports = Object.keys( frontend["ports"] ).length;
+      adapterports += ports;
+      cell2.attr( "rowspan", ports );
+
       for( p in frontend["ports"] )
       {
         port = frontend["ports"][p];
-        tvdata.push( { "adapter": adapter,
-                       "frontend": frontend,
-                       "port": port,
-                       "source": "" } );
+
+        cell3 = $('<td>');
+        cell3.html( getPort( port ));
+        row.append( cell3 );
+
+        cell4 = $('<td>');
+        cell4.html( getSource( port["source_id"] ));
+        row.append( cell4 );
+
+        row.appendTo( '#plant' );
+        row = $('<tr>');
       }
+
     }
+    cell.attr( "rowspan", adapterports );
   }
-
-  var data =
-  {
-    datatype: "array",
-    localdata: tvdata
-  };
-
-  var theme = getTheme();
-
-  $("#name").addClass('jqx-input');
-  $("#type").addClass('jqx-input');
-  if( theme.length > 0 )
-  {
-    $("#name").addClass('jqx-input-' + theme);
-    $("#type").addClass('jqx-input-' + theme);
-  }
-
-  var dataAdapter = new $.jqx.dataAdapter(data);
-  $("#gui").jqxGrid(
-    {
-      source: dataAdapter,
-      theme: theme,
-      autoheight: true,
-      enablehover: false,
-      columns: [
-        { text: 'Adapter', datafield: 'adapter', cellsrenderer: renderAdapter },
-        { text: 'Frontend', datafield: 'frontend', cellsrenderer: renderFrontend },
-        { text: 'Port', datafield: 'port', cellsrenderer: renderPort },
-        { text: 'Source', datafield: 'source', cellsrenderer: renderSource },
-      ]
-    });
 }
 
-$(document).ready( ready );
+function getAdapter( adapter )
+{
+  if( adapter["present"] == 1 )
+    imgstyle = "";
+  else
+    imgstyle = "opacity:0.2";
+  if( adapter["path"].indexOf( "/usb" ) != -1 )
+    icon = "<img src=\"images/usb-icon.png\" style=\"" + imgstyle + "\"/>";
+  else if ( adapter["path"].indexOf( "/pci" ) != -1 )
+    icon = "<img src=\"images/pci-icon.png\" style=\"" + imgstyle + "\"/>";
+  return icon + '<br/>' + adapter["name"];
+}
+
+function getFrontend( frontend )
+{
+  return frontend["name"];
+}
+
+function getPort( port )
+{
+  return port["name"];
+}
+
+function getSource( source_id )
+{
+  if( source_id >= 0 )
+    return sources[source_id];
+  else
+    return " - ";
+}
+
