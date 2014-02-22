640970116e37f4ce70bcd2e3c09a7b321d9940ee
Lars Schmohl <lars.schmohl@gmail.com>
2014-02-23 00:03:05 +0100

    improve build script

--- build.sh
+++ build.sh
@@ -9,23 +9,27 @@ die()
   exit -1
 }
 
+cd $SCRIPT_DIR                          || die "change to home failed"
 git submodule update --init --recursive || die "submodule update failed"
 
-cd v4l-utils                            || die "change to v4l-utils failed"
+cd $SCRIPT_DIR/v4l-utils                || die "change to v4l-utils failed"
 git checkout master                     || die "checkout v4l-utils failed"
 git reset --hard origin/master          || die "reset v4l-utils failed"
-git am -3 ../patches/*                  || die "patching v4l-utils failed"
+git pull                                || die "pull v4l-utils failed"
+git am -3 $SCRIPT_DIR/patches/*         || die "patching v4l-utils failed"
 cd $SCRIPT_DIR                          || die "change to home failed"
 
-cd tsdecrypt                            || die "change to tsdecrypt failed"
+cd $SCRIPT_DIR/tsdecrypt                || die "change to tsdecrypt failed"
 git checkout master                     || die "checkout tsdecrypt failed"
+git reset --hard origin/master          || die "reset tsdecrypt failed"
+git pull                                || die "pull tsdecrypt failed"
 git submodule update --init --recursive || die "tsdecrypt submodule update failed"
 cd $SCRIPT_DIR                          || die "change to home failed"
 
-cd v4l-utils                            || die "change to v4l-utils failed"
+cd $SCRIPT_DIR/v4l-utils                || die "change to v4l-utils failed"
 autoreconf -vfis                        || die "autoreconf v4l-utils failed"
 ./configure                             || die "configure v4l-utils failed"
-cd lib/libdvbv5                         || die "change to libdvbv5 failed"
+cd $SCRIPT_DIR/v4l-utils/lib/libdvbv5   || die "change to libdvbv5 failed"
 make                                    || die "make libdvbv5 failed"
 cd $SCRIPT_DIR                          || die "change to home failed"
 
